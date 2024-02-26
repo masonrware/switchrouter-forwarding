af5189bc9b5968952557b02d3b53ff5f0ee4575d
masonrware <86129546+masonrware@users.noreply.github.com>
2024-02-26 12:36:01 -0600

    potentialchecksum error

--- assign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -259,7 +259,7 @@ public class Router extends Device
 		// Compute the checksum
 		int accumulation = 0;
 		for (int i = 0; i < headerLength * 2; ++i) {
-			accumulation += (0xff & headerData[i * 2]) << 8 | (0xff & headerData[i * 2 + 1]);
+			accumulation += (0xffff & headerData[i * 2]) << 8 | (0xffff & headerData[i * 2 + 1]);
 		}
 		accumulation = ((accumulation >> 16) & 0xffff) + (accumulation & 0xffff);
 		accumulation += (accumulation >> 16);
